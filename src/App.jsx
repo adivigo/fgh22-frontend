41ffd6e9a335836a1b097c99a38d60e6d4f6f826
Adivigo Khalishtama <khalishtama25@gmail.com>
2024-10-29 15:03:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <nav className="px-32 flex justify-between items-center h-24 shadow-lg">
+        <div className="text-3xl">TixIT</div>
+        <ul>
+          <li className="flex gap-14 text-sm">
+            <a href="#">Home</a>
+            <a href="#">Movie</a>
+            <a href="#">Buy Ticket</a>
+          </li>
+        </ul>
+        <div className="flex gap-3 text-sm">
+          <a
+            href="#"
+            className="px-4 py-3 bg-white rounded-lg text-black border border-gray"
+          >
+            SignIn
+          </a>
+          <a
+            href="#"
+            className="px-4 py-3 bg-gray rounded-lg text-white border border-gray"
+          >
+            Sign Up
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      grey: "#EDEDED",
+      red: "#DA0037",
+      gray: "#444444",
+      dark: "#171717",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
