d7a22720bfbff51a0ab310b4881d212773b25c35
Adivigo Khalishtama <khalishtama25@gmail.com>
2024-11-05 13:33:46 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        fetch: resolve(__dirname, "fetch.html"),
-        key: resolve(__dirname, "filename.html"),
-      },
       external: [
         "/src/assets/images/checkLogo.png",
         "/src/assets/images/chatLogo.png",
